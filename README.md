d702df11eb2d0d68026e0fff90d703454e6cb198
sudo-alex1990 <alexrfarnes@gmail.com>
2021-05-19 15:04:43 +0800

    Refactor the code by removing unneccesary variables and simplified the main function

---
+++ README.md
@@ -0,0 +1,54 @@
+# Date Picker App
+
+> A date picker app implemented from scratch with the date-fns library.
+
+## Table of contents
+
+- [General info](#general-info)
+- [Screenshots](#screenshots)
+- [Built With](#built-with)
+- [Features](#features)
+- [Status](#status)
+- [Inspiration](#inspiration)
+- [Contact](#contact)
+
+## General info
+
+Practice the use of parcel bundler and an external library to bundle it. Use the date-fns library to implement the logic and functionality of a date picker from scratch.
+
+## Screenshots
+
+![Example screenshot](./images/screenshot.png)
+[Demo]()
+
+## Technologies
+
+- HTML
+- CSS
+- JavaScript
+- Parcel Bundler
+- date-fns library
+
+## Features
+
+List of features ready and TODOs for future development
+
+- Initially shows the current date
+- A button to display the calendar
+- Two arrow buttons to navigate to previous or next months and update accordingly the calendar
+- It also displays the days of the week
+- Dates from other months are shown to fill in the space just like any calendar
+
+## Status
+
+Project is: _finished_.
+
+## Inspiration
+
+Project based on [JavaScript Simplified by Kyle Cook](https://javascriptsimplified.com/)
+
+## Contact
+
+- GitHub [@AlexRFarnes](https://github.com/AlexRFarnes)
+- Twitter [@AlexRFarnes](https://twitter.com/alexrfarnes)
+- Linkedin [@AlexRFarnes](https://www.linkedin.com/in/alexrfarnes/)
--- script.js
+++ script.js
@@ -2,11 +2,14 @@ import {
   format,
   subMonths,
   addMonths,
-  parse,
-  getDaysInMonth,
   startOfMonth,
   endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
 } from 'date-fns';
+import isSameDay from 'date-fns/isSameDay';
+import isSameMonth from 'date-fns/isSameMonth';
 
 // DOM elements
 const datePickerButton = document.querySelector('.date-picker-button');
@@ -18,87 +21,68 @@ const datePickerDates = document.querySelector('.date-picker-grid-dates');
 
 const monthFormat = 'MMMM - yyyy';
 
-// Set the current month
-let currentMonth = format(new Date(), monthFormat);
-console.log(currentMonth);
-// Selected date
-let selectedDate = new Date().getDate();
-
-// Display the current date on the button
-datePickerButton.textContent = format(new Date(), 'MMMM do, yyyy');
-// Display the current month
-currentMonthEl.textContent = currentMonth;
+// Set the current date
+let selectedDate = new Date();
+let currentMonth;
 
 // FUNCTIONS
+function displayDateOnButton(selectedDate) {
+  datePickerButton.textContent = format(selectedDate, 'MMMM do, yyyy');
+}
+
+function getMonth(selectedDate) {
+  currentMonth = format(selectedDate, monthFormat);
+}
+
+function displayMonth(currentMonth) {
+  currentMonthEl.textContent = currentMonth;
+}
+
 function getPreviousMonth() {
   // Update the current month by subtracting 1 month
-  currentMonth = format(
-    subMonths(parse(currentMonth, monthFormat, new Date()), 1),
-    monthFormat
-  );
-  // Display the  updated month
-  currentMonthEl.textContent = currentMonth;
+  selectedDate = subMonths(selectedDate, 1);
+  getMonth(selectedDate);
+  displayMonth(currentMonth);
 }
 
 function getNextMonth() {
   // Update the current month by adding 1 month
-  currentMonth = format(
-    addMonths(parse(currentMonth, monthFormat, new Date()), 1),
-    monthFormat
-  );
-  // Display the updated month
-  currentMonthEl.textContent = currentMonth;
+  selectedDate = addMonths(selectedDate, 1);
+  getMonth(selectedDate);
+  displayMonth(currentMonth);
 }
 
 function fillInDays() {
   // Remove all the html from the date picker before filling in the new days
   datePickerDates.innerHTML = '';
 
-  // Get the total days of the current month
-  const daysOfCurrentMonth = getDaysInMonth(
-    parse(currentMonth, monthFormat, new Date())
-  );
-  // Get the day of the week the month starts 0(Sunday) - 6(Saturday)]
-  const startOfCurrentMonth = startOfMonth(
-    parse(currentMonth, monthFormat, new Date())
-  ).getDay();
-  // Get the day of the week the month end [0(Sunday) - 6(Saturday)]
-  const endOfCurrentMonth = endOfMonth(
-    parse(currentMonth, monthFormat, new Date())
-  ).getDay();
+  // When the week of the first day of the month begins
+  const firstWeekStart = startOfWeek(startOfMonth(selectedDate));
+  // When the week of the last day of the month ends
+  const lastWeekEnd = endOfWeek(endOfMonth(selectedDate));
+  const dates = eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
 
-  // Get the total number of days of the previous month
-  const totalDaysOfPreviousMonth = getDaysInMonth(
-    subMonths(parse(currentMonth, monthFormat, new Date()), 1)
-  );
-
-  // For the previous month it start from the start of current month day(0 - 6) - 1 until it gets to 0
-  for (let i = startOfCurrentMonth - 1; i >= 0; i--) {
-    const prevMonthDateEl = document.createElement('button');
-    prevMonthDateEl.classList.add('date', 'date-picker-other-month-date');
-    // Subtract i from the total days of the previous month
-    prevMonthDateEl.textContent = `${totalDaysOfPreviousMonth - i}`;
-    datePickerDates.appendChild(prevMonthDateEl);
-  }
-
-  // Current month days must start at 1 and loop through the current month days [inclusive]
-  for (let i = 1; i <= daysOfCurrentMonth; i++) {
-    const currentMonthDateEl = document.createElement('button');
-    currentMonthDateEl.classList.add('date');
-    if (i == selectedDate) {
-      currentMonthDateEl.classList.add('selected');
+  dates.forEach(date => {
+    const dateButton = document.createElement('button');
+    dateButton.classList.add('date');
+    dateButton.textContent = date.getDate();
+    if (!isSameMonth(date, selectedDate)) {
+      dateButton.classList.add('date-picker-other-month-date');
     }
-    currentMonthDateEl.textContent = i;
-    datePickerDates.appendChild(currentMonthDateEl);
-  }
+    if (isSameDay(date, selectedDate)) {
+      dateButton.classList.add('selected');
+    }
 
-  // Next month days start at 1 and loops only through the remaining days of the week (6 - endOfCurrentMonth)
-  for (let i = 1; i <= 6 - endOfCurrentMonth; i++) {
-    const nextMonthDateEl = document.createElement('button');
-    nextMonthDateEl.classList.add('date', 'date-picker-other-month-date');
-    nextMonthDateEl.textContent = `${i}`;
-    datePickerDates.appendChild(nextMonthDateEl);
-  }
+    dateButton.addEventListener('click', () => {
+      selectedDate = date;
+      datePickerEl.classList.remove('show');
+      fillInDays();
+    });
+    datePickerDates.appendChild(dateButton);
+  });
+  displayDateOnButton(selectedDate);
+  getMonth(selectedDate);
+  displayMonth(currentMonth);
 }
 
 // EVENT LISTENERS
@@ -110,8 +94,6 @@ datePickerButton.addEventListener('click', () => {
 prevMonthButton.addEventListener('click', () => {
   // Get the previous month when clicked
   getPreviousMonth();
-  // Set the selected date to none
-  selectedDate = '';
   // Fill the days of the previous month
   fillInDays();
 });
@@ -119,42 +101,9 @@ prevMonthButton.addEventListener('click', () => {
 nextMonthButton.addEventListener('click', () => {
   // Get the next month when clicked
   getNextMonth();
-  // Set the selected date to none
-  selectedDate = '';
   // Fill the days of the next month
   fillInDays();
 });
 
-// Because buttons are generated every time the month changes use event delegation
-datePickerDates.addEventListener('click', e => {
-  // Check to see if the target of the click is not a date and exit the function
-  if (!e.target.matches('.date')) return;
-
-  // Set the selected date to the button's text
-  selectedDate = e.target.textContent;
-
-  // Check to see if the target is a date from another month
-  if (e.target.matches('.date-picker-other-month-date')) {
-    // If the date selected is greater than 20 then the date is from the previous month
-    if (parseInt(e.target.textContent) > 20) {
-      // Call getPreviousMonth()
-      getPreviousMonth();
-    } else {
-      // Else the date is from the next month and call getNextMonth()
-      getNextMonth();
-    }
-  }
-  // Change the button's text to the selected date
-  datePickerButton.textContent = format(
-    parse(`${selectedDate} - ${currentMonth}`, 'd - MMMM - yyyy', new Date()),
-    'MMMM do, yyyy'
-  );
-  // Hide the date picker
-  datePickerEl.classList.remove('show');
-
-  // Fill in the days with the updated selected date
-  fillInDays();
-});
-
-// Fill in the dats with the current date and month
+// Fill in the dats with the current date
 fillInDays();
